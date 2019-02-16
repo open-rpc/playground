3f25723d9c20f41268b9177cbaef0395c589ab01
shanejonas <jonas.shane@gmail.com>
2019-02-16 12:50:15 -0800

    feat: add schema and results docs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import ReactMarkdown from 'react-markdown';
 import infoTemplate from '@open-rpc/generator-docs/templates/info.template.md.js';
 import serverTemplate from '@open-rpc/generator-docs/templates/server.template.md.js';
 import methodTemplate from '@open-rpc/generator-docs/templates/method.template.md.js';
+import refParser from 'json-schema-ref-parser';
+import schemaToMarkdown from 'json-schema-to-markdown-table';
+
+import './App.css'
 
 const fetchUrlSchemaFile = async (schema) => {
   try {
@@ -32,11 +36,11 @@ export default class App extends React.Component {
     this.interval = setInterval(this.refreshEditorData, 1000);
     this.refreshEditorData();
   }
-  refreshEditorData() {
+  async refreshEditorData() {
     const markers = monaco.editor.getModelMarkers();
     let parsedSchema
     try {
-      parsedSchema = JSON.parse(monaco.editor.getModels()[0].getValue())
+      parsedSchema = await refParser.dereference(JSON.parse(monaco.editor.getModels()[0].getValue()));
     } catch (e) {
     }
 
@@ -59,10 +63,11 @@ export default class App extends React.Component {
           <MonacoJSONEditor onChange={this.setMarkers.bind(this)}/>
         </div>
 
-        <div className='docs' style={{margin: '40px'}}>
+        <div className='docs'>
           {this.state.parsedSchema.info && <ReactMarkdown source={infoTemplate({info: this.state.parsedSchema.info})} />}
           {this.state.parsedSchema.servers && <ReactMarkdown source={serverTemplate({servers: this.state.parsedSchema.servers})} />}
-          {this.state.parsedSchema.methods && <ReactMarkdown source={this.state.parsedSchema.methods.map((m) => methodTemplate({method: m})).join('')} />}
+          {this.state.parsedSchema.methods && <ReactMarkdown source={'## Methods \n\n'} />}
+          {this.state.parsedSchema.methods && <ReactMarkdown source={this.state.parsedSchema.methods.map((m) => methodTemplate({method: m, schemaToMarkdown})).join('')} />}
         </div>
       </div>
     );
