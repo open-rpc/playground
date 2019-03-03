c91b109e4b81fafbc6e9d6d77fb3717b3c1e550e
shanejonas <jonas.shane@gmail.com>
2019-03-02 23:39:09 -0800

    fix: error messages too long
    fixes #25

--- src/JSONValidationErrorList.js
+++ src/JSONValidationErrorList.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core';
 
-export default class JSONValidationErrorList extends React.Component {
+
+const styles = theme => ({
+  list: {
+    listStyle: 'none',
+    color: theme.palette.secondary.main,
+    height: '100%',
+    overflow: 'scroll'
+  },
+  wrapper: {
+    maxHeight: '200px'
+  }
+})
+
+class JSONValidationErrorList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {}
   }
   render() {
+    const { classes } = this.props;
     if (!this.props.markers || this.props.markers.length === 0) return null;
     return (
-      <ul style={{listStyle: 'none', color: 'red'}}>
-        {this.props.markers && this.props.markers.map((marker) => {
-           return (
-             <li key={marker.message}>
-               {marker.startLineNumber}:{marker.startColumn} - {marker.message}
-             </li>
-           );
-        })}
-      </ul>
+      <div className={classes.wrapper}>
+        <ul className={classes.list}>
+          {this.props.markers && this.props.markers.map((marker) => {
+            return (
+              <li key={marker.message}>
+                {marker.startLineNumber}:{marker.startColumn} - {marker.message}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
+
+
+export default withStyles(styles)(JSONValidationErrorList);
