0c584d8dee44629bbce5b994f6536fd84f6398ae
shanejonas <jonas.shane@gmail.com>
2019-03-04 17:40:26 -0800

    fix: rpc.discover

--- src/fetchSchemaFromRpcDiscover.js
+++ src/fetchSchemaFromRpcDiscover.js
@@ -1,11 +1,14 @@
+let idCounter = 0;
 export default async (url) => {
   try {
     const response = await fetch(url, {
       method: "POST",
+      headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
         "jsonrpc": "2.0",
         "method": "rpc.discover",
-        "params": []
+        "params": [],
+        "id": idCounter++
       })
     });
     return await response.json();
