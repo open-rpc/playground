69a5d2b3ac1d62d38ae55a353d00ec778b9a1ea2
shanejonas <jonas.shane@gmail.com>
2019-03-25 16:32:36 -0700

    fix: use meta-schema from npm
    fixes #86
    fixes #76

--- src/App.tsx
+++ src/App.tsx
@@ -148,7 +148,7 @@ export default class App extends React.Component<{}, IState> {
           onChangeUrl={this.handleUrlChange} />
         <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
           { this.state.uiSchema.appBar["ui:splitView"] &&
-            <div style={{ display: "flex", flexDirection: "column", height: "100%", width: "100%" }} >
+            <div style={{ display: "flex", flexDirection: "column", height: "100%", width: "50%" }} >
               <JSONValidationErrorList markers={this.state.markers} />
               <MonacoJSONEditor
                 defaultValue={this.state.defaultValue}
