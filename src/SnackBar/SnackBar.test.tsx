ff9b5c2e23866a67e9a0dee1b7ab7bf82f61346b
Zane Starr <zcstarr@gmail.com>
2019-04-22 14:11:43 -0700

    feat: add snackbar notifications to playground fixes #122

---
+++ src/SnackBar/SnackBar.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {SnackBar, SnackBarNotification, NotificationType} from "./SnackBar";
+it("renders notifications", () => {
+  const div = document.createElement("div");
+  for (const notificationType in NotificationType) {
+    if ( typeof  notificationType === "string") {
+    const type = NotificationType[notificationType] as NotificationType;
+    ReactDOM.render(<SnackBar close={null} notification={{ message: "hello", type}} />, div);
+    expect(div.innerHTML.includes("hello")).toBe(true);
+    ReactDOM.unmountComponentAtNode(div);
+    } else {
+      throw new Error("NotificationType contains mixed types");
+    }
+ }
+});
