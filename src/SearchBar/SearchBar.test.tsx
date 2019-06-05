112caadbb997862e56e131ab3cd6a2d88f094613
shanejonas <jonas.shane@gmail.com>
2019-06-05 16:30:14 -0700

    fix: tests passing

--- src/SearchBar/SearchBar.test.tsx
+++ src/SearchBar/SearchBar.test.tsx
@@ -1,38 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppBar from "./AppBar";
+import SearchBar from "./SearchBar";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<AppBar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it("renders uiSchema logo", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<AppBar uiSchema={{
-    appBar: {
-      "ui:logoUrl": "http://foo.salad",
-    },
-  } as any} />, div);
-  expect(div.innerHTML.includes("foo.salad")).toBe(true);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it("renders uiSchema title", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<AppBar uiSchema={{
-    appBar: {
-      "ui:title": "foobar",
-    },
-  } as any} />, div);
-  expect(div.innerHTML.includes("foobar")).toBe(true);
+  ReactDOM.render(<SearchBar />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it("renders uiSchema inputPlaceholder", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<AppBar uiSchema={{
+  ReactDOM.render(<SearchBar uiSchema={{
     appBar: {
       "ui:inputPlaceholder": "enter url",
     },
