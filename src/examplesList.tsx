a3ddfe907dc940cbfe42a38d555091b6a59e82ff
shanejonas <jonas.shane@gmail.com>
2019-06-11 12:56:55 -0700

    fix: default to petstore

---
+++ src/examplesList.tsx
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "api-with-examples",
+    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/api-with-examples-openrpc.json", //tslint:disable-line
+  },
+  {
+    name: "link-example",
+    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/link-example-openrpc.json", //tslint:disable-line
+  },
+  {
+    name: "params-by-name-petstore",
+    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/params-by-name-petstore-openrpc.json", //tslint:disable-line
+  },
+  {
+    name: "petstore-expanded",
+    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/petstore-expanded-openrpc.json", //tslint:disable-line
+  },
+  {
+    name: "petstore",
+    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/petstore-openrpc.json", //tslint:disable-line
+  },
+  {
+    name: "simple-math",
+    url: "https://raw.githubusercontent.com/open-rpc/examples/master/service-descriptions/simple-math-openrpc.json", //tslint:disable-line
+  },
+];
